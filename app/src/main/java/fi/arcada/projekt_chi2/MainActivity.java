6666fd3734eeba1948bee7dafeae4ab5a785fc88
niskanjo <niskanjo@arcada.fi>
2023-04-25 17:51:06 +0300

    Final commit (INLÄMNING)

--- app/src/main/java/fi/arcada/projekt_chi2/MainActivity.java
+++ app/src/main/java/fi/arcada/projekt_chi2/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
+
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -17,7 +17,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+
 
 
 
